308ff426fccae3c009da08ae77675c6cbdf0b4e5
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-08-16 18:42:37 +0300

    add task

--- src/components/NewComponents.tsx
+++ src/components/NewComponents.tsx
@@ -12,15 +12,26 @@ type TopCarsType = {
 
 export const NewComponents = (props: NewComponentsType) => {
     return (
-        <ul>
+        // <ul>
+        //     {props.cars.map((elem) => {
+        //         return (
+        //             <li key={elem.id}>
+        //                 <span>{elem.manufacturer}</span>
+        //                 <span>   Model: {elem.model}</span>
+        //             </li>
+        //         )
+        //     })}
+        // </ul>
+        <table>
             {props.cars.map((elem) => {
-                return (
-                    <li key={elem.id}>
-                        <span>{elem.manufacturer}</span>
-                        <span>   Model: {elem.model}</span>
-                    </li>
-                )
+                return(
+                <tr key={elem.id}>
+                    <th>{elem.manufacturer}</th>
+                    <th>  Model: {elem.model}</th>
+                 </tr>
+                ) 
             })}
-        </ul>
+          
+        </table>
     )
 }
